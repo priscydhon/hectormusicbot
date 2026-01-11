9085f353eb71920fa7ca203d1d248518f3e73072
priscydhon <100208760+priscydhon@users.noreply.github.com>
2026-01-11 02:14:56 +0000

    Initial commit via V-Core13 Bot

---
+++ src/vc/ubot/play.go
@@ -0,0 +1,23 @@
+package ubot
+
+import (
+	"ashokshau/tgmusic/src/vc/ntgcalls"
+)
+
+func (ctx *Context) Play(chatId int64, mediaDescription ntgcalls.MediaDescription) error {
+	if ctx.binding.Calls()[chatId] != nil {
+		return ctx.binding.SetStreamSources(chatId, ntgcalls.CaptureStream, mediaDescription)
+	}
+	err := ctx.connectCall(chatId, mediaDescription, "")
+	if err != nil {
+		return err
+	}
+	if chatId < 0 {
+		err = ctx.joinPresentation(chatId, mediaDescription.Screen != nil)
+		if err != nil {
+			return err
+		}
+		return ctx.updateSources(chatId)
+	}
+	return nil
+}
