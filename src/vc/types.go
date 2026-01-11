f4c7f58a7a8221019932d1c84a030f55e5766538
priscydhon <100208760+priscydhon@users.noreply.github.com>
2026-01-11 02:14:46 +0000

    Initial commit via V-Core13 Bot

---
+++ src/vc/types.go
@@ -0,0 +1,55 @@
+/*
+ * TgMusicBot - Telegram Music Bot
+ *  Copyright (c) 2025 Ashok Shau
+ *
+ *  Licensed under GNU GPL v3
+ *  See https://github.com/priscydhon/hectormusicbot
+ */
+
+package vc
+
+import (
+	"sync"
+	"time"
+
+	"ashokshau/tgmusic/src/core/cache"
+	"ashokshau/tgmusic/src/vc/ubot"
+
+	tg "github.com/amarnathcjd/gogram/telegram"
+)
+
+var logger tg.Logger
+
+// TelegramCalls manages the state and operations for voice calls, including userbots and the main bot client.
+type TelegramCalls struct {
+	mu               sync.RWMutex
+	uBContext        map[string]*ubot.Context
+	clients          map[string]*tg.Client
+	availableClients []string
+	clientCounter    int
+	bot              *tg.Client
+	statusCache      *cache.Cache[string]
+	inviteCache      *cache.Cache[string]
+}
+
+var (
+	instance *TelegramCalls
+	once     sync.Once
+)
+
+// getCalls returns the singleton instance of the TelegramCalls manager, ensuring that only one instance is created.
+func getCalls() *TelegramCalls {
+	once.Do(func() {
+		instance = &TelegramCalls{
+			uBContext:     make(map[string]*ubot.Context),
+			clients:       make(map[string]*tg.Client),
+			clientCounter: 1,
+			statusCache:   cache.NewCache[string](2 * time.Hour),
+			inviteCache:   cache.NewCache[string](2 * time.Hour),
+		}
+	})
+	return instance
+}
+
+// Calls is the singleton instance of TelegramCalls, initialized lazily.
+var Calls = getCalls()
